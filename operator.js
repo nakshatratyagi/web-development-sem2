c4adf353c8835c3bcf04df557ec123b5a893da5b
nakshatra <tyaginakshatra3@gmail.com>
2026-03-09 09:08:01 +0530

    update

---
+++ operator.js
@@ -0,0 +1,33 @@
+let a=9
+let b=6
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a%b);
+console.log(a++);
+console.log(++a);
+console.log(a+=3);
+console.log(a/=3);
+console.log(a*=3);
+console.log(a-=3);
+console.log(a);
+
+// == is value comparison and (===) is used for data type comparison
+let c=45
+let d="45"
+console.log(c==d);
+console.log(c===d);
+(a>18)?"minor":"major"
+day=3
+switch(day){
+    case 1:
+        console.log("monday");
+    case 2:
+        console.log("tuesaday");
+    case 3:
+        console.log("sat");
+    case 4:
+        console.log("sun");
+
+
+}
---
+++ script.js
@@ -0,0 +1,15 @@
+//primitive data types
+let name="alie";
+console.log(typeof name);
+let student={ //non primitive
+    name:"nk",
+    age:25,
+    isstudent: true,
+    email:"mailmail"
+}
+console.log(student);
+let age=30;
+console.log(typeof age);
+//bool,undefined,null,symbol,
+ let isstudent=true;
+ console.log(typeof isstudent);
